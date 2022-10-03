05a4914837a183c8c3c63b9ba5de0d118baed58b
Ingri Quevedo <k_therinquevedo@hotmail.com>
2022-10-02 20:00:07 -0400

    Add functions

--- python/get.py
+++ python/get.py
@@ -1,54 +1,273 @@
 ### Program to get Youtube API data
 ### @ IQ
+
+#py -m pip install --upgrade pip
+#py -m pip install numpy
+#py -m pip install pandas
+#py -m pip install matplotlib
+#pip install --upgrade google-api-python-client
+#pip install --upgrade google-auth-oauthlib google-auth-httplib2
+#py -m pip install seaborn
+#py -m pip install isodate
+#py -m pip install isodate nltk
+#py -m pip install isodate wordcloud
+
 #====================================================================================================================#
 # Preliminaries
 #====================================================================================================================#
 #-------------------------------------------------------------------------------------------------------------------#
 # Libraries
 #-------------------------------------------------------------------------------------------------------------------#
-from timeit import default_timer as timer
+
+#Basic libraries
 import pandas as pd
+import numpy as np
+
+#Paths
 import os
 from pathlib import Path
+from sys import api_version
+
+#Timer
+from timeit import default_timer as timer
+from dateutil import parser
+import isodate
+from datetime import datetime, timedelta
+
+# Google API
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+import googleapiclient.errors
+from googleapiclient.errors import HttpError
+from googleapiclient.discovery import build
+
+# Data visualization libraries
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import seaborn as sns
+sns.set(style="darkgrid", color_codes=True)
+
+# NLP libraries
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+nltk.download('stopwords')
+nltk.download('punkt')
+from wordcloud import WordCloud
 
 #====================================================================================================================#
-# Path
+# My Path
 #====================================================================================================================#
 
 __file__ = 'get.py'
 codefile_directory=Path(__file__).absolute().parent
 print(codefile_directory)
-myroot=codefile_directory.parent
-print(myroot)
-
-output_folder=str(myroot)+"\\"
-logs_folder = str(myroot)+"\\"+'master_programs\\comtrade\\logs'
-# Check whether the logs directory path exists or not
-isExist = os.path.exists(logs_folder)
-if not isExist:
-  # Create directory because it does not exist 
-  os.makedirs(logs_folder)
-  print("The logs directory is created!")
-
-data_folder=str(myroot)+"\\"+'Master_Dataset\\Tariffs_Trade\\data\\comtrade'
-# Check whether the data directory path exists or not
-isExist = os.path.exists(data_folder)
-if not isExist:
-  # Create directory because it does not exist 
-  os.makedirs(data_folder)
-  print("The logs directory is created!")
-
 
 #====================================================================================================================#
 # Parameters
 #====================================================================================================================#
 
-apikey = "AIzaSyDt29H6_4ZRFulkdEbeb-FC5tO3Hoh3zHQ"
+api_key = 'AIzaSyDt29H6_4ZRFulkdEbeb-FC5tO3Hoh3zHQ'
+api_service_name="youtube"
+api_version="v3"
 
+#Get credentials and create an API client
+youtube = googleapiclient
+
+channel_ids = ['UCtYLUTtgS3k1Fg4y5tAhLbw', # Statquest
+               'UCCezIgC97PvUuR4_gbFUs5g', # Corey Schafer
+               'UCfzlCWGWYyIQ0aLC5w48gBQ', # Sentdex
+               'UCNU_lfiiWBdtULKOw6X0Dig', # Krish Naik
+               'UCzL_0nIe8B4-7ShhVPfJkgw', # DatascienceDoJo
+               'UCLLw7jmFsvfIVaUFsLs8mlQ', # Luke Barousse 
+               'UCiT9RITQ9PW6BhXK0y2jaeg', # Ken Jee
+               'UC7cs8q-gJRlGwj4A8OmCmXg', # Alex the analyst
+               'UC2UXDak6o7rBm23k3Vv5dww', # Tina Huang
+              ]
+
+youtube = build('youtube', 'v3', developerKey=api_key)
+    
 #====================================================================================================================#
 # Functions
 #====================================================================================================================#
+def get_channel_stats(youtube, channel_ids):
+    """
+    Get channel statistics: title, subscriber count, view count, video count, upload playlist
+    Params:
+    
+    youtube: the build object from googleapiclient.discovery
+    channels_ids: list of channel IDs
+    
+    Returns:
+    Dataframe containing the channel statistics for all channels in the provided list: title, subscriber count, view count, video count, upload playlist
+    
+    """
+    all_data = []
+    request = youtube.channels().list(
+                part='snippet,contentDetails,statistics',
+                id=','.join(channel_ids))
+    response = request.execute() 
+    
+    for i in range(len(response['items'])):
+        data = dict(channelName = response['items'][i]['snippet']['title'],
+                    subscribers = response['items'][i]['statistics']['subscriberCount'],
+                    views = response['items'][i]['statistics']['viewCount'],
+                    totalVideos = response['items'][i]['statistics']['videoCount'],
+                    playlistId = response['items'][i]['contentDetails']['relatedPlaylists']['uploads'])
+        all_data.append(data)
+    
+    return pd.DataFrame(all_data)
 
+def get_video_ids(youtube, playlist_id):
+    """
+    Get list of video IDs of all videos in the given playlist
+    Params:
+    
+    youtube: the build object from googleapiclient.discovery
+    playlist_id: playlist ID of the channel
+    
+    Returns:
+    List of video IDs of all videos in the playlist
+    
+    """
+    
+    request = youtube.playlistItems().list(
+                part='contentDetails',
+                playlistId = playlist_id,
+                maxResults = 50)
+    response = request.execute()
+    
+    video_ids = []
+    
+    for i in range(len(response['items'])):
+        video_ids.append(response['items'][i]['contentDetails']['videoId'])
+        
+    next_page_token = response.get('nextPageToken')
+    more_pages = True
+    
+    while more_pages:
+        if next_page_token is None:
+            more_pages = False
+        else:
+            request = youtube.playlistItems().list(
+                        part='contentDetails',
+                        playlistId = playlist_id,
+                        maxResults = 50,
+                        pageToken = next_page_token)
+            response = request.execute()
+    
+            for i in range(len(response['items'])):
+                video_ids.append(response['items'][i]['contentDetails']['videoId'])
+            
+            next_page_token = response.get('nextPageToken')
+        
+    return video_ids
+
+def get_video_details(youtube, video_ids):
+    """
+    Get video statistics of all videos with given IDs
+    Params:
+    
+    youtube: the build object from googleapiclient.discovery
+    video_ids: list of video IDs
+    
+    Returns:
+    Dataframe with statistics of videos, i.e.:
+        'channelTitle', 'title', 'description', 'tags', 'publishedAt'
+        'viewCount', 'likeCount', 'favoriteCount', 'commentCount'
+        'duration', 'definition', 'caption'
+    """
+        
+    all_video_info = []
+    
+    for i in range(0, len(video_ids), 50):
+        request = youtube.videos().list(
+            part="snippet,contentDetails,statistics",
+            id=','.join(video_ids[i:i+50])
+        )
+        response = request.execute() 
+
+        for video in response['items']:
+            stats_to_keep = {'snippet': ['channelTitle', 'title', 'description', 'tags', 'publishedAt'],
+                             'statistics': ['viewCount', 'likeCount', 'favouriteCount', 'commentCount'],
+                             'contentDetails': ['duration', 'definition', 'caption']
+                            }
+            video_info = {}
+            video_info['video_id'] = video['id']
+
+            for k in stats_to_keep.keys():
+                for v in stats_to_keep[k]:
+                    try:
+                        video_info[v] = video[k][v]
+                    except:
+                        video_info[v] = None
+
+            all_video_info.append(video_info)
+
+def get_comments_in_videos(youtube, video_ids):
+    """
+    Get top level comments as text from all videos with given IDs (only the first 10 comments due to quote limit of Youtube API)
+    Params:
+    
+    youtube: the build object from googleapiclient.discovery
+    video_ids: list of video IDs
+    
+    Returns:
+    Dataframe with video IDs and associated top level comment in text.
+    
+    """
+    all_comments = []
+    
+    for video_id in video_ids:
+        try:   
+            request = youtube.commentThreads().list(
+                part="snippet,replies",
+                videoId=video_id
+            )
+            response = request.execute()
+        
+            comments_in_video = [comment['snippet']['topLevelComment']['snippet']['textOriginal'] for comment in response['items'][0:10]]
+            comments_in_video_info = {'video_id': video_id, 'comments': comments_in_video}
+
+            all_comments.append(comments_in_video_info)
+            
+        except: 
+            # When error occurs - most likely because comments are disabled on a video
+            print('Could not get comments for video ' + video_id)
+        
+    return pd.DataFrame(all_comments)     
+
+
+
+
+scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
+
+def main():
+    # Disable OAuthlib's HTTPS verification when running locally.
+    # *DO NOT* leave this option enabled in production.
+    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+    api_service_name = "youtube"
+    api_version = "v3"
+    client_secrets_file = "YOUR_CLIENT_SECRET_FILE.json"
+
+    # Get credentials and create an API client
+    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+        client_secrets_file, scopes)
+    credentials = flow.run_console()
+    youtube = googleapiclient.discovery.build(
+        api_service_name, api_version, credentials=credentials)
+
+    request = youtube.channels().list(
+        part="snippet,contentDetails,statistics",
+        id="UC_x5XG1OV2P6uZZ5FSM9Ttw"
+    )
+    response = request.execute()
+
+    print(response)
+
+if __name__ == "__main__":
+    main()
 #--------------------------------------------------------------------------------------------------------------------#
 # PART A. Functions that download the Comtrade Bulk data 
 #--------------------------------------------------------------------------------------------------------------------#
